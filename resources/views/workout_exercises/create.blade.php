7b48cec3f3d2097fa8ba9b0621110ea653666993
Scott Knoll <scknoll88@gmail.com>
2023-04-08 20:16:35 -0500

    wip

--- resources/views/workout_exercises/create.blade.php
+++ resources/views/workout_exercises/create.blade.php
@@ -1,12 +1,19 @@
 <x-app-layout>
+	<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Add Exercise
+        </h2>
+    </x-slot>
+
 	<x-container>
-		<div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-			<h1>Add Exercise</h1>
+		<div class="mx-auto py-4">
 			<form action="/workouts/{{ $workout->id }}/exercises" method="post">
 				@csrf
 				<div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
 					<label for="exercise" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Exercise</label>
 					<div class="mt-2 sm:col-span-2 sm:mt-0 mb-4">
+					{{-- <input type="exercise_id" name="body_weight" id="body_weight" autocomplete="body_weight" value="{{ old('body_weight')}}" class="block w-full max-w-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" placeholder="lbs"> --}}
+					{{-- </div> --}}
 						<select name="exercise_id">
 							<option value=""></option>
 							@foreach ($exercises as $exercise)
